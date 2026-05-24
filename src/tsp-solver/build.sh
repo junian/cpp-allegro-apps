af450c1d6e526d6239da9584654c7ba918fe144b
Junian Triajianto <author@junian.dev>
2026-05-24 14:19:06 +0700

    chore(tsp-solver): Clean up legacy build files and standardize project structure

--- src/tsp-solver/build.sh
+++ src/tsp-solver/build.sh
@@ -2,5 +2,5 @@
 # Build script for macOS and Linux
 # Requires: pkg-config, allegro 5
 
-g++ "Travelling Salesman Problem Solver.cpp" -o tsp-solver \
+g++ "main.cpp" -o tsp_solver.exe \
     $(pkg-config allegro-5 allegro_main-5 allegro_font-5 allegro_ttf-5 allegro_primitives-5 --libs --cflags)
